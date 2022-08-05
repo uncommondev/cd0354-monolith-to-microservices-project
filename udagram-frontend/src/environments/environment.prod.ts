db831606e01527b57d8cc8c13bf40708863c69ea
Uncommon Dev <uncommondev344@gmail.com>
2022-08-05 10:23:53 +0100

    Updating Build for Frontend
    Decided to go with the LoadBalancer approach (why not?) and have refactored the configuration to make sure it reach the reverse proxy.
    
    Spotted a potential issue whereby building the image locally on an M1 Mac can cause errors so I am updating Travis to build the image with a V2 tag.
    
    Link to StackOverflow with the possible cause: https://stackoverflow.com/a/71102144

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a3836eebce7eb40e996bc5362007c40a-792511406.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
